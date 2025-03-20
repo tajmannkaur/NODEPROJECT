c5fd6dbcbb72c09561a8e22fa52e5216484f3d33
tajmannkaur <tajmannkamboj08@gmail.com>
2025-03-20 13:51:04 -0600

    update

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = require("./routes/pages");
 
 
-const app= express();
-const PORT=3000;
+const app = express();
+const PORT = 3000;
 
 //implementing routes
 app.use(router);
