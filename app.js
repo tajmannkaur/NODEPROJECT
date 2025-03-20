9ef4a16a3477a34d001e809faa38b1943faba6a9
tajmannkaur <tajmannkamboj08@gmail.com>
2025-03-20 12:14:25 -0600

    update

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = require("./routes/pages");
+
+
+const app= express();
+const PORT=3000;
+
+//implementing routes
+app.use(router);
+
+app.listen(PORT, ()=>{
+
+console.log(`server is running on http://localhost:${PORT}`);
+});
---
+++ routes/pages.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get("/", (req, res) =>{
+    res.send("This is the home page!");
+});
+
+router.get("/abpout",(req, res)=>{
+    res.send("This is the home page!");
+
+});
+
+
+module.exports = router;
